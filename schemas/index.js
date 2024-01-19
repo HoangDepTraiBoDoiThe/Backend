06eb14f6d98cf693377e039edff317579cb49278
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-01-19 08:11:10 +0700

    ShareMe_backend

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import User from "./User"
+import Pin from "./Pin"
+import Comment from "./Comment"
+import Save from "./Save"
+import PostedBy from "./PostedBy"
+export const schemaTypes = [User, Pin, Comment, Save, PostedBy]
